5dbae7cada92d28a1b8994bef180c646f2e85ee9
james7132 <james.siqi.liu@gmail.com>
2015-02-22 23:29:09 -0500

    Small fixes from breaking the project earlier

--- Assets/External Libraries/UnityUtilLib/MeshPostProcessor.cs
+++ Assets/External Libraries/UnityUtilLib/MeshPostProcessor.cs
@@ -3,7 +3,8 @@ using UnityEditor;
 
 class MeshPostprocessor : AssetPostprocessor {
 
-	void OnPreprocessModel () { 
-		(assetImporter as ModelImporter).globalScale = 1f; 
+	void OnPreprocessModel () {
+		//Currently broken
+		//(assetImporter as ModelImporter).globalScale = 1f; 
 	}
 } 
