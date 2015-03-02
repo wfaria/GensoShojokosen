6c6e2d0c28cf6b917e5103a7b31fd37e28d75df0
james7132 <james.siqi.liu@gmail.com>
2015-03-02 12:44:48 -0500

    Hurtboxes and Hurtbox Visualization

---
+++ Assets/Scripts/DrawHurtbox.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DrawHurtbox : MonoBehaviour {
+
+	void Awake() {
+#if UNITY_EDITOR
+		//Continue
+#else
+		Destroy (this);
+#endif
+	}
+
+	void OnDrawGizmos() {
+		Gizmos.color = Color.yellow;
+		CircleCollider2D[] hurtboxes = GetComponentsInChildren<CircleCollider2D> ();
+		for (int i = 0; i < hurtboxes.Length; i++) {
+			Gizmos.DrawSphere(hurtboxes[i].transform.position, hurtboxes[i].radius);
+		}
+	}
+}
---
+++ Assets/Scripts/Hurtbox.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hurtbox : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D collider) {
+		SendMessageUpwards ("OnHurtboxHit");
+	}
+}
